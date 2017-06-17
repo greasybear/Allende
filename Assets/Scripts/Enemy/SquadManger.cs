ee89376a82e0046bc1679bd462b0b4a068fca75a
greasybear <captainfalconlink@hotmail.com>
2017-06-17 13:24:42 -0500

    Alarm Commit
    changed level layout, fixed navmesh spacing issue, set up alarm system

--- Assets/Scripts/Enemy/SquadManger.cs
+++ Assets/Scripts/Enemy/SquadManger.cs
@@ -13,7 +13,14 @@ public class SquadManger : MonoBehaviour {
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
     public Image alarmImage;
+    SquadManger squadManager;
+    //public GameObject alarmSpawners;
+    //public Transform alarmSpawnPosition;
 
+
+
+   // void SpawnAfterAlarm()
+   // { Instantiate(alarmSpawners, alarmSpawnPosition); }
 
     void BreakPatrol()
     {   if (!alarmSounded)
@@ -25,7 +32,7 @@ public class SquadManger : MonoBehaviour {
     foreach (GameObject enemy in squadMembers)
             {
              
-                enemy.GetComponent<Patrol>().breakPatrol = true;            
+                enemy.GetComponent<Patrol>().breakPatrol = true;            //null here
             }
     }
 
@@ -40,29 +47,30 @@ public class SquadManger : MonoBehaviour {
         if (alarmer !=null)
         {
             float dist = Vector3.Distance(alarm.position, alarmer.transform.position); //getting null reference here, MAYBE USE TRIGGER BOOL
-            if (dist<3f)
+            if (dist<2f)
             { alarmSounded = true; }
         }
         
-        if (alarmSounded)
+        if (alarmSounded && alarmer !=null)
         {
-            alarmer.GetComponent<EnemyMovmentSlow>().alarmSearch = true;
+            alarmer.GetComponent<EnemyMovmentSlow>().alarmSearch = true; //null here
             alarmer.GetComponent<EnemyMovmentSlow>().FindPlayer();
         }
 
-
-        foreach (GameObject enemy in squadMembers)
-        {
-            if (enemy.GetComponent<Patrol>().breakPatrol)
+      
+            foreach (GameObject enemy in squadMembers)
+            {
+                if (enemy.GetComponent<Patrol>().breakPatrol)  //null here
                 {
 
 
                     BreakPatrol();
                 }
+       
+           
             }
-            
-
-        
+            //if (alarmSounded)
+            //    { Invoke("SpawnAfterAlarm", 5); }
 
     }
 }
