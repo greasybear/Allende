6a7bf363c5742079164d26f3b860e671a9fc034a
greasybear <captainfalconlink@hotmail.com>
2017-07-08 16:49:30 -0500

    Rocket Commit
    fixed splash damage, still needs condition of exploding

--- Assets/Scripts/Player/rocketScript.cs
+++ Assets/Scripts/Player/rocketScript.cs
@@ -7,61 +7,55 @@ public class rocketScript : MonoBehaviour {
     public int damage;
     float timer;
     public GameObject explosion;
-    bool exploding;
     Renderer render;
+    public float radius = 10f;
+    public Collider[] damageRadius;
+    bool exploded;
 
-    
+
     void OnCollisionEnter(Collision col)
     {
-      //  render = GetComponent<Renderer>();
-
         if (col.gameObject.tag == "Enemy")
         {
-            exploding = true;
+            exploded = true;            
             col.gameObject.GetComponent<EnemyHealthSlowGun>().TakeDamageObject(damage);
             GameObject exp = Instantiate(explosion, transform.position, Quaternion.identity);
-            Destroy(exp, 3);
-            //render.enabled = false;
+
+           
+
             Destroy(gameObject);
-            //spawn particle system
+
+            Destroy(exp, 3);
+                     
         }
-        
-        
-        
-        
+                
         else if (col.gameObject.tag != "Player")
             {
-            exploding = true;
-            Destroy(gameObject);
-            //spawn particle sysetem
+
+            exploded = true;
+
             GameObject exp = Instantiate(explosion, transform.position, Quaternion.identity);
-            //render.enabled = false;
+            
             Destroy(exp, 3);
-            Destroy(gameObject, 3);
+            Destroy(gameObject);
         }
     }
-
-
-    //void OnTriggerEnter(Collider enemy)
-    //{
-    //    if (enemy.gameObject.tag == "Enemy")
-    //    {
-    //        if (exploding)
-    //        { enemy.gameObject.GetComponent<EnemyHealthSlowGun>().TakeDamageObject(damage); }
-
-    //    }
-
-    //}
-
-
-
     void Update()
     {
             timer += Time.deltaTime;
 
             if (timer > 5)
             { Destroy(gameObject); }
-        
+
+        damageRadius = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider badGuy in damageRadius)
+           
+           { if (badGuy.tag == "Enemy")
+            {
+                badGuy.gameObject.GetComponent<EnemyHealthSlowGun>().TakeDamageObject(damage);
+            }
+           }
+
     }
 
 }
